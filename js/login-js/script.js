c28e04f778696f2cd1aca59c9428ed9224cc88aa
Felipe Ramalho <91812562+framalho400@users.noreply.github.com>
2022-11-23 21:31:57 -0300

    23/11

--- js/login-js/script.js
+++ js/login-js/script.js
@@ -3,40 +3,57 @@ const Icpf = document.getElementById('cpf');
 const Isenha = document.getElementById('senha');
 
 
+const logar = document.querySelector('#entrar').addEventListener('click',
+    function (e) {
+        e.preventDefault();
+    entrar();
+})
 
 
-const url = 'http://10.92.198.40:8080/api/usuario/login'
+
+const url = 'http://localhost:8080/api/usuario/login'
+
+usuario =
+{
+    cpf: Icpf.value,
+    senha: Isenha.value
+}
+
 
 function entrar() {
-    usuario =
-    {
-        cpf: Icpf.value,
-        senha: Isenha.value
-    }
-    axios.post(url, usuario).then((resp) => {
+    axios.post(url, usuario, {
+             
+    headers: {
+        'Content-Type': 'application/json',
+        'accept': 'application/json',
+        Authorization: `Bearer ${token}` }
+}
+    )
+    .then((resp) => {
 
-    resp.json().then((resposta) => {
-        onsole.log(JSON.stringify(response.data));
+    resp.json()
+    .then((resposta) => {
+        msgErro(msgText="Erro ao logar", color= "red");
         sessionStorage.setItem("token", resposta.token);
-        const token = parseJwt(resposta.token);
-        alert(token);
-        
-        window.location.href = "../../template/usuario/home.html"; 
+        const token = resposta.token;
+        console.log(JSON.stringify(response.data));
+        window.location.href = "../../templates/usuario/home.html"; 
         
     }).catch((error) => {
         alert("Erro ao fazer login");
         /*  window.location.href = "../suporte/index.html"; */
         console.log(error)
-    /*     if (resp.status == 401) {
-            msg = "Usuário ou senha incorretos"
-            exibeErro(msg)
-        } */
+        msgErro(msgText="Erro ao logar", color= "red");
+        if (resp.status == 401) {
+            msgErro(msgText="Erro ao logar", color= "red");
+            
+        } 
     })
 })
 }
 
 
-function parseJwt(token) {
+ function parseJwt(token) {
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
     var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
@@ -44,11 +61,23 @@ function parseJwt(token) {
     }).join(''));
 
     return JSON.parse(jsonPayload);
-};
+}; 
 
-const logar = document.querySelector('#entrar').addEventListener('click',
-function (e) {
+function msgErro(msgText, color) {
 
-    e.preventDefault();
-    entrar();
-})
+    const div = document.createElement('div');
+
+    div.classList.add('msg');
+    div.style.borderLeft = `solid 10px ${color}`;
+    div.innerText = msgText;
+    document.body.appendChild(div);
+
+
+    setTimeout(function () {
+        div.classList.add('close')
+    }, 3000); // 5 segundos
+    setTimeout(function () {
+        div.remove();
+    }, 6000); // 6 segundos
+
+}
